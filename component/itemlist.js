d92f19576421700ffcd3985e07995b19a526d381
Lyqueen <58933390+fikrullaily@users.noreply.github.com>
2021-09-22 11:41:48 +0700

    Update itemlist.js
    langsung filter dari sini

--- component/itemlist.js
+++ component/itemlist.js
@@ -1,37 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import styles from '../styles/Home.module.scss';
 import Item from "./item";
 
 
  const ItemList = (props) => {
+
+  const [searchTerm, setSearchTerm] = useState("");
   const { itemList, handleEdit, handleDelete } = props;
+
+  const handleChange = (e) =>{
+    e.preventDefault();
+    setSearchTerm(e.target.value);
+}
+
   return (
     <div>
-      {itemList?.map((item) => {
-        return (
-          <Item
-            item={item}
-            handleEdit={handleEdit}
-            handleDelete={handleDelete}
-            key={item.id}
-          />
-        );
+      <div className={styles.item}>
+            <input className={styles.search} type="text" placeholder="Search..." 
+                name="search" onChange={handleChange} value={searchTerm} />
+      </div>
+        <br />
+
+      {itemList.filter((item) => {
+        if (searchTerm == ""){
+          return item;
+        } else if (
+          item.title.toLowerCase().includes(searchTerm.toLowerCase())
+        ) {
+          return item
+        }
+        }).map((item) => {
+          return <Item item={item} key={item.id} handleEdit={handleEdit} />;
       })}
-    </div>
+</div>
   );
  };
 
 export default ItemList;
-
-
-
-// const ItemList = ({itemList}) => {
-//     return (
-//         <div>
-//             {itemList.map(item => {
-//                 return (
-//                     <Item item={item} />
-//                 )
-//             })}
-//         </div>
-//     );
-// };
